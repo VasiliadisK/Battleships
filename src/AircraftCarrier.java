52abab996447207355c3359cb6c6c8d876c5a47a
DESKTOP-KR95E4Q\30694 <kostasvasiliadis123@gmail.com>
2021-05-07 17:14:57 +0300

    Μικρές αλλαγές στη κλάση ship και υποκλάσεις

--- src/AircraftCarrier.java
+++ src/AircraftCarrier.java
@@ -5,5 +5,13 @@ public class AircraftCarrier extends Ship{
 	public AircraftCarrier (int ShipXpos, int ShipYpos,boolean isVertical,String aName,int length) {
 		super(ShipXpos,ShipYpos,isVertical,aName,length);
 	}
+	
+	public void PrintShipName () {
+		super.PrintShipName();
+	}
+
 
 }
+
+	
+
